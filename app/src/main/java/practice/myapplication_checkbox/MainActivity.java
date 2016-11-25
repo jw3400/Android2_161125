bdbac60c1857a1ddc75f42a6d9c48cf60b67964e
jy <lee01423@naver.com>
2016-11-25 14:43:21 +0900

    11월25일 실습-check 변형 선택

--- app/src/main/java/practice/myapplication_checkbox/MainActivity.java
+++ app/src/main/java/practice/myapplication_checkbox/MainActivity.java
@@ -1,5 +1,6 @@
 package practice.myapplication_checkbox;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,52 +11,80 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    CheckBox c1;
+    Switch s1;
     LinearLayout Layout1;
     RadioButton r1;
     RadioButton r2;
     RadioButton r3;
     RadioGroup rg;
-    Button b1;
     ImageView image;
+    Button b1;
+    Button b2;
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("애완동물 사진 보기");
+        setTitle("안드로이드 사진 보기");
 
         Layout1 = (LinearLayout) findViewById(R.id.Layout);
-        c1 = (CheckBox) findViewById(R.id.checkBox);
+        s1 = (Switch) findViewById(R.id.switch1);
 
         Layout1.setVisibility(View.GONE);
 
-        c1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+         s1.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                 if (isChecked) {
                     Layout1.setVisibility(View.VISIBLE);
 
-                    b1 = (Button) findViewById(R.id.button);
                     r1 = (RadioButton) findViewById(R.id.radioButton);
                     r2 = (RadioButton) findViewById(R.id.radioButton2);
                     r3 = (RadioButton) findViewById(R.id.radioButton3);
                     rg = (RadioGroup) findViewById(R.id.radioGroup);
                     image = (ImageView) findViewById(R.id.imageView);
 
-                    b1.setOnClickListener(new View.OnClickListener() {
+                    r1.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
                             if (r1.isChecked() == true) {
-                                image.setImageResource(R.drawable.dog);
-                            } else if (r2.isChecked() == true) {
-                                image.setImageResource(R.drawable.cat);
-                            } else
-                                image.setImageResource(R.drawable.rabbit);
+                                image.setImageResource(R.drawable.jellybean);
+                            }
+                        }
+                    });
+
+                    r2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            if (r2.isChecked() == true) {
+                                image.setImageResource(R.drawable.kitkat);
+                            }
+                        }
+                    });
+
+                    r3.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            if (r3.isChecked() == true) {
+                                image.setImageResource(R.drawable.lolipop);
+                            }
                         }
                     });
                 }
@@ -66,9 +95,46 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
 
 
